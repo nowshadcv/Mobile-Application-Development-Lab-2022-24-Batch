c32f2a15dee61067277e1cb5b81e1a547d21c2ee
sahlasherinmk <115775304+sahlasherinmk@users.noreply.github.com>
2023-09-20 14:45:09 +0530

    Add files via upload

---
+++ 49_SAHLA_SHERIN/01-Android Activity Life Cycle/01_MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.SensorAdditionalInfo;
+import android.os.Bundle;
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        private number one;
+        private number two;
+        private number three;
+        private number four;
+        private number five;
+        private number six;
+        private number seven;
+        private number eight;
+        private number nine;
+        setprivate=Addition;
+        setprivate=subtration;
+        setprivate=multiplicatin;
+        setprivate=divition;
+        set
+
+
+
+
+    }
+}
