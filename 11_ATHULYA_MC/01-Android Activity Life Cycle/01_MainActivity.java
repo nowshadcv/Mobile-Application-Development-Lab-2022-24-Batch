db3e48234b7fc106f2745eff2f36e060ce64575c
AthulyaprasannakumarMC <115775602+AthulyaprasannakumarMC@users.noreply.github.com>
2023-09-20 14:46:03 +0530

    Add files via upload

---
+++ 11_ATHULYA_MC/01-Android Activity Life Cycle/01_MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.alc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("lifecycle","onCreate invoked");
+
+
+    }
+
+
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+
+    }
+    protected void onResume()
+    {
+        super.onStart();
+        Log.d("lifecycle","onResume invoked");
+
+    }
+    protected void onPause()
+    {
+        super.onStart();
+        Log.d("lifecycle","onPause invoked");
+
+    }
+    protected void onStop()
+    {
+        super.onStart();
+        Log.d("lifecycle","onStop invoked");
+
+    }
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+
+    }
+
+
+    protected void onDestroy()
+    {
+        super.onStart();
+        Log.d("lifecycle","onDestroy invoked");
+
+    }
+
+
+}
