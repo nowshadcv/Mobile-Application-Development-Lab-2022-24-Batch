87b4011c4c6016d7b0a7b7e1a63804302af8d319
Safa sidhi <115776259+safasidhi@users.noreply.github.com>
2023-11-08 15:48:13 +0530

    Add files via upload

---
+++ 47_SAFA_SIDHI/06-Toggle an image/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.toggleanimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ImageView i1,i2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i1=(ImageView) findViewById(R.id.Image1);
+        i2=(ImageView) findViewById(R.id.Image2);
+        i1.setOnClickListener(this);
+        i2.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+     if(v.getId()==R.id.Image1){
+      i1.setVisibility(View.GONE);
+      i2.setVisibility(View.VISIBLE);
+    }
+     else {
+         i2.setVisibility(View.GONE);
+         i1.setVisibility(View.VISIBLE);
+     }
+     }
+}
