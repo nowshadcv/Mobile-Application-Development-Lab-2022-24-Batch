919beb9a9af664bd7d8e872641549f0cae3e2020
Alfiyafaisalkt <115776061+Alfiyafaisalkt@users.noreply.github.com>
2023-11-14 12:43:49 +0530

    Add files via upload

---
+++ 06_ALFIYA FAISAL/06_Explicit intent/06_SecondActivity.java
@@ -0,0 +1,52 @@
+package com.example.explicitintents;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.explicitintents.databinding.ActivitySecond2Binding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivitySecond2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second3);
+        Bundle bundle=getIntent().getExtras();
+        if(bundle != null){
+            String imgindex= bundle.getString("ImageIndex");
+            setImage(imgindex);
+
+        }
+    }
+    private void setImage(String index){
+        ImageView imageView=(ImageView) findViewById(R.id.imageViewCountry);
+        switch(index){
+            case "1":
+                imageView.setImageResource(R.drawable.india);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.sa);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.nz);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.aus);
+                break;
+        }
+    }
+}
