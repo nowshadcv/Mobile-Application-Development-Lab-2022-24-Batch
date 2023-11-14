1f1e9012ed457e4694a35eb81e7b4641faf6983c
sahlasherinmk <115775304+sahlasherinmk@users.noreply.github.com>
2023-11-14 12:29:10 +0530

    Add files via upload

---
+++ 49_SAHLA_SHERIN/07-Explicit Intents/07_MainActivity2.java
@@ -0,0 +1,42 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle bundle=getIntent().getExtras();
+        if (bundle !=null){
+            String imgindex=bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+    private void setImage(String index) {
+        ImageView imageView = (ImageView) findViewById(R.id.imageviewcountry);
+        switch (index) {
+            case "1":
+                imageView.setImageResource(R.drawable.india);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.sa);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.nz);
+                break;
+            case "4":
+                imageView.setImageResource(R.drawable.aus);
+                break;
+
+
+        }
+    }}
+
+
+
