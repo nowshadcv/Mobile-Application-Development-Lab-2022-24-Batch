b7488b8f9f171503e32d315067542b5cb2b0425a
AthulyaprasannakumarMC <115775602+AthulyaprasannakumarMC@users.noreply.github.com>
2023-11-22 16:04:40 +0530

    Add files via upload

---
+++ 04_AISWARYA/09_Array Adapter with ListView/09-MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.arrayadaptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+      String fruitlist []={"apple","orange","banana","pomegranate","dates"};
+      ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.activity_listview,R.id.textview,fruitlist);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("ListView","Item is clicked @position"+position);
+            }
+        });
+    }
+}
