e3ce4848cac27be2ce1a40825cc78fed56ac9444
Athila N <115775426+AATHILA@users.noreply.github.com>
2023-12-05 12:41:40 +0530

    Add files via upload

---
+++ 10_ATHILA/09-ArrayAdapter/09_MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewAutoScrollHelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    String fruitlist[] ={"apple","orange","banana","pomegranate","dates"};
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listview);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.activity_main2,R.id.texview,fruitlist);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position,long l) {
+
+                if(position==0){
+                    startActivity(new Intent(MainActivity.this,apple.class));
+                }
+                else if(position==1){
+                    startActivity(new Intent(MainActivity.this,orange.class));
+                }
+                else if(position==2){
+                    startActivity(new Intent(MainActivity.this,banana.class));
+                }
+                else if(position==3){
+                    startActivity(new Intent(MainActivity.this,pomegranate.class));
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this,dates.class));
+                }
+                Log.i("ListView","Item is clicked @position" + position);
+            }
+        });
+    }
+}
