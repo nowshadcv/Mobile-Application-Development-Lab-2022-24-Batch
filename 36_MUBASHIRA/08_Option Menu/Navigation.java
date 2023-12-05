36f9c106b1cb58e6da5b3718f90968c2dd85df63
mubashirakv <115776109+mubashirakv@users.noreply.github.com>
2023-12-05 12:59:26 +0530

    Add files via upload

---
+++ 36_MUBASHIRA/08_Option Menu/Navigation.java
@@ -0,0 +1,8 @@
+package com.example.optionmenu;
+
+public class Navigation {
+
+
+    public static NavController findNavController(MainActivity mainActivity, int nav_host_fragment_content_main) {
+    }
+}
